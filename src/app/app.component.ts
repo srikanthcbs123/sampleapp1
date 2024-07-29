3de9070239f5b3ab0c188fcc9fdc01576929a42d
srikanthcbs123 <srikanthcbs123@gmail.com>
2024-07-29 20:11:59 +0530

    Fixed some of the issues

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {NgForm} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'sampleapp1';
+  title = 'angTempForms';
+  mycolor:string="green";
+  mycolor1:string="";
+  mycolor2:string="class3"
+  country:string='';
+  b:boolean=false;
+  @ViewChild('f',{static:false}) signUpForm?:NgForm;
+
+  genders=['Male','FeMale'];
+  Cities:string[]=["newdelhi","newyork","newyear","chenni","vizag","Australia"];
+  onSubmit(form:NgForm){
+this.mycolor="yellow";
+this.mycolor1="class2";
+    debugger;
+    console.log(form.value.username);
+     console.log(form.value.email);
+     console.log(form.value.secret);
+     console.log(form.value.answer);
+     console.log(form.value.gender);
+
+  }
 }
